956e320d452089a2a91d86ab959f1b632e5fc27d
achyuth reddy <achyuthreddy@achyuths-MacBook-Pro.local>
2021-08-10 11:18:14 +0530

    implemented the create Parking lot functionality

--- dist/ParkingLot.js
+++ dist/ParkingLot.js
@@ -1,27 +1,29 @@
 class ParkingLot {
-    constructor() {
-        this.maxParkingSlots = 0;
-        this.parkingSlots = new Array();
+  constructor() {
+    this.maxParkingSlots = 0
+    this.parkingSlots = new Array()
+  }
+  createParkingLot(input) {
+    if (!input || input <= 0)
+      return "please enter the valid to allot the no of parking slots"
+    this.maxParkingSlots = input
+    if (this.maxParkingSlots === 0)
+      return "the number of parking slots entered should be more than zero "
+    for (let i = 0; i < this.maxParkingSlots; i++) {
+      this.parkingSlots.push(null)
     }
-    createParkingLot(input) {
-        console.log(input);
-        if (this.maxParkingSlots === 0)
-            return "the number of parking slots entered should be more than zero ";
-        for (let i = 0; i < this.maxParkingSlots; i++) {
-            this.parkingSlots.push(null);
-        }
-        return this.parkingSlots;
-    }
-    parkCar() { }
-    unParkCar() { }
-    getAllPrakingStatus() { }
-    getSlotByCarNo() { }
-    getAllCarNumbersByColor() { }
-    getAllSlotsByCarColor() { }
-    getNextNearestSlot() { }
+    return this.parkingSlots
+  }
+  parkCar() {}
+  unParkCar() {}
+  getAllPrakingStatus() {}
+  getSlotByCarNo() {}
+  getAllCarNumbersByColor() {}
+  getAllSlotsByCarColor() {}
+  getNextNearestSlot() {}
 }
-const parkingObj = new ParkingLot();
-const abc = parkingObj.createParkingLot(4);
-console.log("abc", abc);
-module.exports = ParkingLot;
+const parkingObj = new ParkingLot()
+const abc = parkingObj.createParkingLot(4)
+console.log(abc)
+module.exports = ParkingLot
 //# sourceMappingURL=ParkingLot.js.map
--- tests/parkingLot.test.ts
+++ tests/parkingLot.test.ts
@@ -6,7 +6,24 @@ describe("Testing all the methods in the praking lot classes", function () {
   describe("creating the Parking lot", () => {
     test("No maximum parking lot size given ,passes", () => {
       const newParkingLot = new ParkingLot()
-      expect(newParkingLot.createParkingLot()).toEqual([])
+      expect(newParkingLot.createParkingLot()).toEqual(
+        "please enter the valid to allot the no of parking slots"
+      )
+    })
+    test("Negative maximum parking lot size given ,passes", () => {
+      const newParkingLot = new ParkingLot()
+      expect(newParkingLot.createParkingLot(-45)).toEqual(
+        "please enter the valid to allot the no of parking slots"
+      )
+    })
+    test("valid maximum parking lot size given ,passes", () => {
+      const newParkingLot = new ParkingLot()
+      expect(newParkingLot.createParkingLot(4)).toEqual([
+        null,
+        null,
+        null,
+        null,
+      ])
     })
   })
 })
