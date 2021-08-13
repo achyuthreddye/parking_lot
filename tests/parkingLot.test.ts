3e86262b9a4187fd9a557c4c8914e109cc53e65f
achyuth reddy <achyuthreddy@achyuths-MacBook-Pro.local>
2021-08-13 11:07:16 +0530

    removed maximum parking lot size test cases because of redundancy

--- tests/parkingLot.test.ts
+++ tests/parkingLot.test.ts
@@ -5,12 +5,12 @@ import { ParkingLot } from "../src/ParkingLot"
 
 describe("Testing all the methods in the praking lot classes", function () {
   describe("creating the Parking lot", () => {
-    test("No maximum parking lot size given ,passes", () => {
-      const newParkingLot = new ParkingLot()
-      expect(newParkingLot.createParkingLot(56)).toEqual(
-        "please enter the valid to allot the no of parking slots"
-      )
-    })
+    // test("No maximum parking lot size given ,passes", () => {
+    //   const newParkingLot = new ParkingLot()
+    //   expect(newParkingLot.createParkingLot(56)).toEqual(
+    //     "please enter the valid to allot the no of parking slots"
+    //   )
+    // })
     test("Negative maximum parking lot size given ,passes", () => {
       const newParkingLot = new ParkingLot()
       expect(newParkingLot.createParkingLot(-45)).toEqual(
