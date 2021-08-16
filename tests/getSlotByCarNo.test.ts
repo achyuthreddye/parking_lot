db3c7e55bf3502f97c5223bf14997ad3536ead50
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-08-16 11:02:00 +0530

    added the description for the getSlotByCarNo

--- tests/getSlotByCarNo.test.ts
+++ tests/getSlotByCarNo.test.ts
@@ -11,10 +11,10 @@ describe("checking for the slot number based on the car number", () => {
     carNumber: "KA40M8502",
     carColor: "red",
   })
-  test("should ", () => {
+  test("should return the slot id where the car is parked", () => {
     expect(newParkingLot.getSlotByCarNo("KA40M8501")).toEqual(1)
   })
-  test("should ", () => {
+  test("should return the status of car not parked ", () => {
     expect(newParkingLot.getSlotByCarNo("KA40M8504")).toEqual(
       "car is not parked"
     )
