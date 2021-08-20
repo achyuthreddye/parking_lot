bc1ec9732bfae9b3d57f4bdbd9aa8d704024a095
Achyuth Reddy <achyuth.reddy@everest.engineering>
2021-08-20 15:40:54 +0530

    Implemented the wrong file error

--- src/fileConsole.ts
+++ src/fileConsole.ts
@@ -6,6 +6,12 @@ export function fileConsole(
   parkingLot: ParkingLot,
   commandLineInputs: string[]
 ) {
+  if (!fs.existsSync(commandLineInputs[2])) {
+    console.log(
+      "file doesn't exists Please enter the valid filename using ./fileInputs.txt is encouraged"
+    )
+    return
+  }
   fs.readFile(commandLineInputs[2], "utf-8", (err, data) => {
     if (err) console.log("error in reading file")
     const inputArray = data.split("\n")
